b9170900cf9e9dcf9a61cce94451bb54d0759a86
Neil Sproston <neil.sproston@Mac2403.local>
2017-07-18 13:36:55 +0100

    Chnage script to only specific set of address data - by date

--- LOAD_GB_AWS.sh
+++ LOAD_GB_AWS.sh
@@ -13,15 +13,15 @@ result=$(mysql -e "exit"  2>&1)|| log "Cannot connect to database: $result"
 
 #s3="s3://app-olcs-pri-olcs-deploy-s3/addressbase/"
 s3="s3://devapp-olcs-pri-olcs-deploy-s3/addressbase/"
-addressbaseversion="2015-09-15"
+addressbaseversion="2017-04-13"
 
 result=$(mysql < ./create_gb.sql 2>&1) || log "Failed to run create_gb.sql: $result"
 
 result=$(mysql -e "ALTER TABLE address_gb DISABLE KEYS;" 2>&1) || log "DISABLE KEYS Failed: $result"
 
-result=$(/usr/local/bin/aws s3 ls ${s3}| grep csv.zip 2>&1) || log "Failed to list files from ${s3}: $result"
+result=$(/usr/local/bin/aws s3 ls ${s3}| grep ${addressbaseversion} | grep csv.zip 2>&1) || log "Failed to list files from ${s3}: $result"
 
-files=( $(/usr/local/bin/aws s3 ls ${s3} | grep csv.zip | cut -d' ' -f5 2>&1)) || log "Failed to extract file names"
+files=( $(/usr/local/bin/aws s3 ls ${s3} | grep ${addressbaseversion} | grep csv.zip | awk '{print $4}' 2>&1)) || log "Failed to extract file names"
 
 [ ${#files[@]} -gt 0 ] || log "No Files Found!"
 
@@ -29,7 +29,7 @@ for f in ${files[*]}
 do
   #grab fresh key to protect from 1 hour timeout
   source /tmp/s3assume.sh "arn:aws:iam::054614622558:role/ADDRESS-ProdToDev-AssumeRole"
-  
+
   echo "file:"
   echo $f
   echo $(date '+%H:%M:%S') Downloading "$f"
